b556d37014078d2b07836dd557a825d66bac9389
Komo_249 <kmomuphnie@gmail.com>
2017-02-22 08:55:51 -0500

    Update the_normal_one.c

--- bubble_sort/the_normal_one.c
+++ bubble_sort/the_normal_one.c
@@ -1,18 +1,13 @@
-
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-
-
-	
     const int n = 10;
     int sequence[n] = {12,17,3141,423,23,45,1,234,904,13};
     int count = 0;
     int sequenceSorted[n];
     int pivot = 0;
     int k = n - 1;
-    
     
     while(k){
         for (int i = 0; i < n - 1; ++i)
@@ -31,8 +26,6 @@ int main(int argc, char const *argv[])
         }
         sequenceSorted[n - 1] = sequence[n - 1];
         k = k - 1;
-        
-        
     }
     
     for(int j = 0; j < n; j++){
