d2838124c4c8ec418fa64119f10d3fef5a8c31a2
Komo_249 <kmomuphnie@gmail.com>
2017-02-22 09:06:32 -0500

    Update GEB_interview.c

--- GEB/GEB_interview.c
+++ GEB/GEB_interview.c
@@ -1,51 +1,81 @@
-("(Background:Earlier this year, a highly parallel IBM computer system named “Watson” played a massively publicized game of the television quiz show Jeopardy against two champion human players, winding up crushing them both. Some commentators described Watson’s (or rather, IBM’s) accomplishment as a major step toward the computer understanding of language. )
+("(Background:Earlier this year, a highly parallel IBM computer system named “Watson” played a massively publicized 
+ game of the television quiz show Jeopardy against two champion human players, winding up crushing them both. 
+ Some commentators described Watson’s (or rather, IBM’s) accomplishment as a major step toward the computer understanding of language. )
 
-Q:Hi, Professor Hofstadter. We know that you are here at U of I to give a speech about IBM computer system “Watson”. Is that your recent research orientation?
+Q:Hi, Professor Hofstadter. We know that you are here at U of I to give a speech about IBM computer system “Watson”. 
+Is that your recent research orientation?
 
 
-A: No, I have to say over the last thirty years, I’ve always been interested in trying to be aware of what is going on in world of computer modes of thinking and I ‘ve always wanted to know whether things that gives a lot of press merit that press or not. I’ve taught courses over the years. I taught a course called Hype versus Hope, you know in Artificial Intelligience, in that course we tried to figure out the extent to which whether a lot of highly-touted programs should be taken seriously or not. I’ve been interested in this question and I organized some symposia on this topic over the years. But I wouldn’t call that my major part of my research. It’s more been a kind of worry on my part. I’ve always been worried that artificial Intelligience unpsychologically realistic. We’ve managed to do things that seems very “human”, but we do it in a “cheap” technology and I don’t like that idea and I’ve always trying to find out..
+A: No, I have to say over the last thirty years, I’ve always been interested in trying to be aware of what is going on in world
+of computer modes of thinking and I ‘ve always wanted to know whether things that gives a lot of press merit that press or not.
+I’ve taught courses over the years. I taught a course called Hype versus Hope, you know in Artificial Intelligience, in that course
+we tried to figure out the extent to which whether a lot of highly-touted programs should be taken seriously or not.
+I’ve been interested in this question and I organized some symposia on this topic over the years. But I wouldn’t call that my
+major part of my research. It’s more been a kind of worry on my part. I’ve always been worried that artificial Intelligience
+unpsychologically realistic. We’ve managed to do things that seems very “human”, but we do it in a “cheap” technology and
+I don’t like that idea and I’ve always trying to find out..
 
 
 Q:So you mean there are systematic differences between how our brain really works and how they try to simulate the brain?
 
 
-A: I wouldn’t say that simulate the brain, but programs try to do some of the activities. So in the case of Watson, they tried to get Watson to be able to respond to questions written in English about as they called it “trivial”. Some of the claims made are that this is the deep nature language understanding. I think there is nothing  like that. That is not true. That’s what my talk was about. I am just trying to give people a prospective much more skeptical then the press.
+A: I wouldn’t say that simulate the brain, but programs try to do some of the activities. So in the case of Watson,
+they tried to get Watson to be able to respond to questions written in English about as they called it “trivial”.
+Some of the claims made are that this is the deep nature language understanding. I think there is nothing  like that.
+That is not true. That’s what my talk was about. I am just trying to give people a prospective much more skeptical then the press.
 
 
-Q: Yeah, you mentioned a lot of interesting questions in the lecture, like “Does President Obama have prime number of friends?”, do you find youself enjoy doing what you did?
+Q: Yeah, you mentioned a lot of interesting questions in the lecture, like “Does President Obama have prime number of friends?”,
+do you find youself enjoy doing what you did?
 
 
-A:Yeah, we’ve made a lot of questions that Watson would not be able to answer or understand at all. And it was a lot of fun making those questions. ……
+A:Yeah, we’ve made a lot of questions that Watson would not be able to answer or understand at all. And it was a lot of fun making
+those questions. ……
 
 
 Q:Do you think we can realize the dream of Artificial Intelligience?
 
 
-A: I don’t foward to, it troubles me. But I think in principle it’s possible that computers or devices… I don’t like the word “computers: because it seems to me there going to have to be things that have a lot of autonomy, but whatever they are, it seems to me that in the long run, devices, I wouldn’t call them computers. I don’t like the word “robot” either, it has certain connotations. Robots make you think of a big metallic objects that goes in a row.(with hands waving)
+A: I don’t foward to, it troubles me. But I think in principle it’s possible that computers or devices…
+I don’t like the word “computers: because it seems to me there going to have to be things that have a lot of autonomy,
+but whatever they are, it seems to me that in the long run, devices, I wouldn’t call them computers.
+I don’t like the word “robot” either, it has certain connotations. Robots make you think of a big metallic objects that goes in a row.
+(with hands waving)
 
 
 Q:Just like in your book, you choose the word “soul” rather than “conciousness”?
 
 
-A: Yeah, sometimes I use that word. But in any cases, artificial devices that has its own goals have to struggle in the world, you know, sometimes having successes and sometimes having failures and creating over a long period of time a modern of itself. You could imagine a computer exploring itself, not knowing anything about itself at all, but watching itself doing certain things.
+A: Yeah, sometimes I use that word. But in any cases, artificial devices that has its own goals have to struggle in the world,
+you know, sometimes having successes and sometimes having failures and creating over a long period of time a modern of itself.
+You could imagine a computer exploring itself, not knowing anything about itself at all, but watching itself doing certain things.
 
 
 Q: It’s like halting problem?
 
 
-A: Yeah, a little bit. Of course it get to paradoxical, but you can’t get into paradoxical. But what I mean is that if a computer were to gain knowledge about its own strength and own weaknesses over a long period time and build up modern of itself, I’d successed. I don’t see that why they couldn’t be like us.
+A: Yeah, a little bit. Of course it get to paradoxical, but you can’t get into paradoxical. But what I mean is that if a computer
+were to gain knowledge about its own strength and own weaknesses over a long period time and build up modern of itself,
+I’d successed. I don’t see that why they couldn’t be like us.
 
 
 Q: So is that your version of Turing’s test? Like Hofstadter version of Turing’s test?
 
 
-A: No, I don’t replace turing’s test by any kind of tests. I mean it seems to me that Turing’s idea was that ask a device that is hidden to you and interact with language. It seems to me that a good way trying to figure out whether it is intelligient…
+A: No, I don’t replace turing’s test by any kind of tests. I mean it seems to me that Turing’s idea was that ask a device
+that is hidden to you and interact with language. It seems to me that a good way trying to figure out whether it is intelligient…
 
 
 Q: And this kind of test is very deep.
 
 
-A: Yeah, very deep. Very very profound. You might haven’t seen my other books. I’ve got a book published in 1997, it got a french title Le Ton beau de Marot.  And I had a dialogue, an interrogator interacting with a program. It just takes place where the Turing’s test should take place. So there are two participants DRH and AMT, means Douglas Richard Hofstadter and Alan Mathison Turing. But I gave them different meanings, DRH=Dull Rigid Human and AMT=ACE mechanical Translator. ACE is the first computer that Turing has made. It’s a very funny dialogue, it’s extremely funny and shows how deep, as you said, is the Turing’s test, how complex the questions can be and how complex the answers can be.
+A: Yeah, very deep. Very very profound. You might haven’t seen my other books. I’ve got a book published in 1997,
+it got a french title Le Ton beau de Marot.  And I had a dialogue, an interrogator interacting with a program.
+It just takes place where the Turing’s test should take place. So there are two participants DRH and AMT,
+means Douglas Richard Hofstadter and Alan Mathison Turing. But I gave them different meanings,
+DRH=Dull Rigid Human and AMT=ACE mechanical Translator. ACE is the first computer that Turing has made.
+It’s a very funny dialogue, it’s extremely funny and shows how deep, as you said, is the Turing’s test,
+how complex the questions can be and how complex the answers can be.
 
 
 
@@ -57,7 +87,9 @@ A: Yeah, very deep. Very very profound. You might haven’t seen my other books.
 Q: I think you really love analogy.
 
 
-A: I do. I meant any human love. What people don’t understand is that they are constantly making analogies. They don’t understand that all words are made of analogy. They don’t understand that they make analogy ten times a second. Every word, every microact of cognition is an analogy.
+A: I do. I meant any human love. What people don’t understand is that they are constantly making analogies.
+They don’t understand that all words are made of analogy. They don’t understand that they make analogy ten times a second.
+Every word, every microact of cognition is an analogy.
 
 
 Q: So you mean analogy is the bases of our brain or how it works?
@@ -83,7 +115,11 @@ A: I think it’s a nightmare cause it’s frightening because the old thing peo
 Q: Like in physics?
 
 
-A:No, no. I mean, you know Kurzweil? Maybe you don’t familiar with him. The singularity is the notion that in a few decades, because of Moore’s Law, the constant acceleration of the speed and sizes of computation, that idea that sooner or later, within 40 years or 60 years, there will be absolute point of which human mind will be overtaken by computers and then computers being smarter than us and would be able to create their own successors even faster so the spiral totally out of control.
+A:No, no. I mean, you know Kurzweil? Maybe you don’t familiar with him. The singularity is the notion that in a few decades,
+because of Moore’s Law, the constant acceleration of the speed and sizes of computation, that idea that sooner or later,
+within 40 years or 60 years, there will be absolute point of which human mind will be overtaken by computers
+and then computers being smarter than us and would be able to create their own successors even faster
+so the spiral totally out of control.
 
 
 Q: So the point where human intelligience equals to computer intelligience is singularity?
@@ -95,7 +131,10 @@ A: That’s correct. And so the idea is that we would be left in the dust. We wo
 Q: Just like in the Science fiction? Human are dominated by computers?
 
 
-A: Yeah, we would be left in the dust, we would be like dogs, like *&*^(didn’t understand), like bacteria. (laugh) and these other beings would be our successors. And I don’t know, to me that’s sad. That’s frightening, that’s not the “dream” of me. I don’t call it the dream of AI, I call it the nightmare of AI. But there are a lot of people look foward to that. They think that’s great. Because they want to be immortal.
+A: Yeah, we would be left in the dust, we would be like dogs, like *&*^(didn’t understand), like bacteria. (laugh)
+and these other beings would be our successors. And I don’t know, to me that’s sad. That’s frightening, that’s not the “dream” of me.
+I don’t call it the dream of AI, I call it the nightmare of AI. But there are a lot of people look foward to that.
+They think that’s great. Because they want to be immortal.
 
 
 Q: That sounds evil…
